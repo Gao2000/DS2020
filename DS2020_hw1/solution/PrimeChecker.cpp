3b58aa4323e81b280a9dd7c5d02fcb34133fbe0a
Gao2000 <kcj89039.eed07@nctu.edu.tw.com>
2020-10-07 13:27:29 +0800

    final_solution commit

--- DS2020_hw1/solution/PrimeChecker.cpp
+++ DS2020_hw1/solution/PrimeChecker.cpp
@@ -49,7 +49,9 @@ unsigned long PRIMECHECKER::PrimeChecker(unsigned long a, unsigned long b) {
   unsigned long prime_num = 0;
   
   for(; a <= b; a++) {
-    if(isPrime(a)) prime_num++; //check the number is prime or not
+    int m = a % 6;
+    if(m == 1 || m == 5)
+      if(isPrime(a)) prime_num++; //check the number is prime or not
   }
 	
 	return prime_num; //return the prime_num's qutantity
