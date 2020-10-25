09cc50a1dd51ef3fa7ed21214bcbd85184c7f34d
Gao2000 <kcj89039.eed07@nctu.edu.tw.com>
2020-10-25 18:00:07 +0800

    hw2-int-nonsimplest

--- DS2020_hw2/HW2_Int/test.c
+++ DS2020_hw2/HW2_Int/test.c
@@ -3,28 +3,31 @@
 #include <math.h>
 #include <stdbool.h>
 
+
+//int version
 struct chain{
   int n; //highest degree
-  double *an; //coffient of polynomial
+  int *an; //coffient of polynomial
 };
 
 void creat_chain(struct chain * c){
   int i, n;
-  double ai;
+  int ai;
   
   printf("please enter the highest degree of polynomial:");
   scanf("%d", &n);
   (*c).n = n;
-  (*c).an = (double *)calloc(n+1, sizeof(double));
+  (*c).an = (int *)calloc(n+1, sizeof(double));
 
   for(int i = n; i >= 0; i--){
-    scanf("%lf", &ai);
+    scanf("%d", &ai);
     (*c).an[i] = ai;
   }
 }
 
 void show(struct chain c){
-  int i = c.n;
+  int i = c.n; //degree
+
   printf("\n");
   while(i >= 0){
     if((i != c.n) && (c.an[i] >= 0))
@@ -32,13 +35,13 @@ void show(struct chain c){
     switch(i)
     {
       case 1:
-        printf("%.2lfX", c.an[i]);
+        printf("%dX", c.an[i]);
         break;
       case 0:
-        printf("%.2lf", c.an[i]);
+        printf("%d", c.an[i]);
         break;
       default:
-        printf("%.2lfX^%d", c.an[i], i);
+        printf("%dX^%d", c.an[i], i);
         break;
     }
     i--;
@@ -48,16 +51,74 @@ void show(struct chain c){
 
 void check_changes(struct chain *r){
   int i = (*r).n;
-  while((fabs((*r).an[i]) <= 1e-5) && (i >= 0))
+  while((abs((*r).an[i]) <= 0) && (i >= 0))
     i--;
   (*r).n = i;
 }
 
 bool check_zero(struct chain r){
   for(int i = 0; i <= r.n; i++)
-    if(fabs(r.an[i]) >= 1e-6)
+    if(abs(r.an[i]) >= 0)
       return false;
   return true;
+}
+
+int gcd(int i, int j){
+  int shift = 0;
+
+  //GCD(0,j) == j, GCD(i,0) == i, GCD(0,0) == 0
+  if(i == 0) return j;
+  if(j == 0) return i;
+
+  //Let shift := lg K, where K is the greatest power of 2 dividing both i and j.
+  while(((i | j) & 1) == 0){
+    shift++;
+    i >>= 1;
+    j >>= 1;
+  }
+  while((i & 1) == 0)
+    i >>= 1;
+
+  //From here on, i is always odd.
+  do{
+    //remove all factor of 2 in v-- they are not common
+    //note: v is not zero, so while will terminate
+    while((j & 1) == 0)
+      j >>= 1;
+
+    /*Now i and j are both odd. Swap if necssary so i <= j, then set j = j - i
+     * (which is even). For bignums, the swapping is just pointer movement, and
+     * the subtraction can be done in-place
+     */
+    if(i > j){
+      int t = i; i = j; j = t; //Swap i and j
+    }
+
+    j -= i; //Here j >= i
+  }while(j != 0);
+
+  //restore common factors of 2
+  return i << shift;
+}
+
+void simplest(struct chain *r){
+  int n = (*r).n;
+  int i = (*r).an[n];
+  printf("%d\n", i);
+  for(int j = n - 1; j >= 0; j--)
+    if((*r).an[j] != 0){
+      printf("%d\n", (*r).an[j]);
+      i = gcd(abs(i), abs((*r).an[j]));
+    }
+
+  printf("GCD: %d\n", i);
+  
+  for(int j = n; j >= 0; j--)
+    (*r).an[j] /= i;
+
+  printf("ans:");
+  show((*r));
+  printf("\n\n");
 }
 
 void do_div(struct chain ch1, struct chain ch2) //Eculidean algorithim ch1 = q * ch2 + r
@@ -70,9 +131,9 @@ void do_div(struct chain ch1, struct chain ch2) //Eculidean algorithim ch1 = q *
     exit(0);
   }
   
-  if(ch1.an[ch1.n] < ch2.an[ch2.n])
-    for(int k = 0; k <= ch1.n; k++)
-      ch1.an[k] *= ch2.an[ch2.n];
+  //if(ch1.an[ch1.n] < ch2.an[ch2.n])
+  for(int k = 0; k <= ch1.n; k++) //keep ch1.n % ch2.n == 0 
+    ch1.an[k] *= ch2.an[ch2.n];
 
   printf("////////////////////////////\nch1:");
   show(ch1);
@@ -82,8 +143,9 @@ void do_div(struct chain ch1, struct chain ch2) //Eculidean algorithim ch1 = q *
 
   q.n = ch1.n - ch2.n;
   r.n = ch1.n - 1;
-  q.an = (double*)calloc(q.n+1, sizeof(double));
-  r.an = (double*)calloc(r.n+1, sizeof(double));
+  q.an = (int*)calloc(q.n+1, sizeof(int));
+  r.an = (int*)calloc(r.n+1, sizeof(int));
+
   while(i <= q.n){
     r.n = ch1.n - 1;
     q.an[q.n - i] = ch1.an[ch1.n]/ch2.an[ch2.n];
@@ -107,8 +169,12 @@ void do_div(struct chain ch1, struct chain ch2) //Eculidean algorithim ch1 = q *
   printf("===========================\n");
 
   check_changes(&r);
+  if(ch2.an[ch2.n] < 0)
+    for(int j = ch2.n; j >= 0; j--)
+      ch2.an[j] *= -1;
 
   if(r.n == -1){
+    simplest(&ch2);
     show(ch2);
     free(r.an);
   }
