fbb450dfb97868f4b4a519ec69f64d8a303d219a
Gao2000 <kcj89039.eed07@nctu.edu.tw.com>
2020-10-26 11:25:47 +0800

    still working

--- DS2020_hw2/GCD.cpp
+++ DS2020_hw2/GCD.cpp
@@ -3,10 +3,38 @@
 // ds_hw2_array_gcd
 //
 #include "GCD.h"
+#include <cmath>
+#include <iostream>
+
+//Eculidean algorithim in1 = q * in2 + r
+void do_div(long *in1, long *in2, int deg1, int deg2){
+  int i = 0, j;
+  long q[1000] = {0}; long r[1000] = {0};
+  int deg_q, deg_r;
+  deg_q = deg1 - deg2;
+  deg_r = deg1 - 1;
+  
+  //if in1's degree need to biger than in2
+  long* tmp;
+  if(deg1 < deg2){
+    tmp = in2;
+    in2 = in1;
+    in1 = temp;
+  }
+
+  if(check_zero(in2, deg2)){
+    cout << "wrong in2 is zero\n";
+    exit(0);
+  }
+ 
+  simplest(in1); simplest(in2);
+  while(i <= deg_n){
+  
+  }
+}
 
 long* GCD::FindGCD(long *in1,long *in2,int deg1,int deg2){
-	
-	
-	
+  do_div(in1, in2, deg1, deg2);	
+	return this.ans;
 }
 
--- DS2020_hw2/GCD.h
+++ DS2020_hw2/GCD.h
@@ -9,11 +9,13 @@
 using namespace std;
 class GCD {
 	private:
-
+    long ans[1000] = {0};
+    int deg_ans;
 	public:
 	GCD(){}
 	~GCD(){}
 	long* FindGCD(long *in1,long *in2,int deg1,int deg2);
+  friend void do_div(long*, long*, int, int);
 };
 #endif
 
